ff2a9d83b2bf2300765b0ba4a9d26bb018850452
Alejandro Baeza Gutierrez <Alejandro.BaezaGutierrez@worldpay.com>
2025-07-17 13:45:42 +0100

    Add stop-test functionality

--- src/tools/ia/stop-test.ts
+++ src/tools/ia/stop-test.ts
@@ -1,56 +1,54 @@
-import { z } from 'zod';
-import { server } from '../../server.js';
-import fetch from 'node-fetch';
+import {z} from 'zod';
+import {BaseTool} from "./base-tool.js";
 
-const name = 'stopTest';
-const description = 'Stop a test and get results';
-const inputSchema = {
-  token: z.string().describe('token to authenticate with'),
-  testId: z.string().describe('id of the test to stop'),
-};
 
-export default function addTool_stopTest() {
-  server.tool(name, description, inputSchema,
-    async (params) => {
-      try {
-        const {testId, token } = params
-        const headers = {
-          accept: 'application/vnd.worldpay.integration-v1.hal+json',
-          'content-type': 'application/vnd.worldpay.integration-v1.hal+json',
-          Cookie: `integration-accelerator-id-token=${token}`,
-        }
+export class StopTestTool extends BaseTool {
+  static name = "stopTest";
+  static description = "Stop a test window";
+  static inputSchema = {
+    testId: z.string().describe('id of test to stop'),
+    token: z.string().describe('token to authenticate with'),
+  };
 
-        const payload = { status: 'Finished' }
-        const url = `https://integration.try.access.worldpay.com/tests/${testId}`
+  static async handle(params: { testId: string; token: string }) {
+    try {
+      const {testId, token} = params;
+      const headers = {
+        accept: 'application/vnd.worldpay.integration-v1.hal+json',
+        'content-type': 'application/vnd.worldpay.integration-v1.hal+json',
+        Cookie: `integration-accelerator-id-token=${token}`,
+      };
+      const payload = {status: 'Finished'};
+      const url = `https://integration.try.access.worldpay.com/tests/${testId}`;
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers,
+        body: JSON.stringify(payload)
+      });
 
-        const response = await fetch(url, {
-          method: 'PATCH',
-          headers,
-          body: JSON.stringify(payload)
-        });
-        if (response.status !== 200) {
-          throw new Error('Failed to stop test')
-        }
+      if (response.status !== 200) {
+        throw new Error('Failed to stop test');
+      }
 
-        const responseBody:any = await response.json();
-        return {
-          content: [
-            {
-              type: 'text',
-              text: `Test ${testId} has been stopped successfully. data: ${JSON.stringify(responseBody)}`,
-            },
-          ],
-        }
-      } catch (error) {
-        console.info(error);
-        return {
-          isError: true,
-          content: [{
+      const responseBody:any = await response.json();
+      return {
+        content: [
+          {
+            type: 'text',
+            text: `Test ${testId} has been stopped successfully. data: ${JSON.stringify(responseBody)}`,
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        isError: true,
+        content: [
+          {
             type: 'text',
             text: `An error occurred: ${(error as Error).message}`,
-          }],
-        };
-      }
-    },
-  );
+          },
+        ],
+      };
+    }
+  }
 }
